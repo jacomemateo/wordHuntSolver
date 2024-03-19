39fa678461b8d47c50e6aeebf44cb7e4815b89f0
Your Name <you@example.com>
2024-03-19 08:43:06 -0400

    trying to implement server

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
-use std::time::Duration;
-use dbus::blocking::Connection;
-use dbus::Message;
-use dbus::message::MatchRule;
+use tokio;
+use dbus_tokio::connection;
+use dbus_crossroads::Crossroads;
 
-// This programs implements the equivalent of running the "dbus-monitor" tool
-fn main() {
-    // First open up a connection to the session bus.
-    let conn = Connection::new_system().expect("D-Bus connection failed");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (resource, conn) = connection::new_system_sync().expect("D-Bus connection failed");
 
-    // Match rule, it left empty it will get every single message and act as dbus-monitor
-    let rule = MatchRule::new().with_interface("com.example.greeting");
+    let _handle = tokio::spawn(async {
+        let err = resource.await;
+        panic!("Lost connection to D-Bus: {}", err);
+    });
 
-    conn.add_match(rule, |_: (), _, msg| {
-        handle_message(&msg);
-        true
-    }).expect("add_match failed");
+    conn.request_name("com.example.dbustest", false, true, false).await?;
 
-    loop {
-        conn.process(Duration::from_millis(1000)).unwrap();
-    };
-}
+    let mut cr = Crossroads::new();
 
-fn handle_message(msg: &Message) {
-    println!("{}", msg.get1::<String>().unwrap() );
+    Ok(())
 }
